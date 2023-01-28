e34a3bb064022149b36f772614db959d35ab1e4d
Arsen Baibulov <arsen@MacBook-Air-Arsen.local>
2023-01-28 05:38:04 +0500

    Shop

--- src/components/creditcard.jsx
+++ src/components/creditcard.jsx
@@ -23,7 +23,7 @@ const Creditcard = (props) => {
             <Cards number={number} name={name} expiry={expiry} cvc={cvc} focused={focus}/>
             <div>
                 <input type="tel" name="number" placeholder='Введите номер карты' value={number} onChange={e=>setNumber(e.target.value)} onFocus={e=>setFocus(e.target.name)} maxLength='16' pattern='[0-9]{16}' required/>
-                <input type="text" name="name" placeholder='Введите имя владельца' value={name} onChange={e=>setName(e.target.value)} onFocus={e=>setFocus(e.target.name)} pattern='[A-Z]' required/>
+                <input type="text" name="name" placeholder='Введите имя владельца' value={name} onChange={e=>setName(e.target.value)} onFocus={e=>setFocus(e.target.name)} required/>
                 <input type="text" name="expiry" placeholder='MM/YY' value={expiry} onChange={e=>setExpiry(e.target.value)} onFocus={e=>setFocus(e.target.name)} maxLength='4' pattern='[0-9]{4}' required/>
                 <input type="tel" name="cvc" placeholder='CVC' value={cvc} onChange={e=>setCvc(e.target.value)} onFocus={e=>setFocus(e.target.name)} maxLength='3' pattern='[0-9]{3}' required/>
             </div>
